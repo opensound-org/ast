691a49cd1984e571244b3acf6f60e9b748588b05
29 <791603901@qq.com>
2024-11-22 09:25:26 +0800

    💥 (MapExt): Add an edge case where the new key is occupied.

--- src/collections.rs
+++ src/collections.rs
@@ -3,14 +3,29 @@ use std::{
     collections::{BTreeMap, HashMap},
     hash::{BuildHasher, Hash},
 };
+use thiserror::Error;
+
+/// Error returned by `MapExt::replace_key`.
+#[derive(Error, Debug, Copy, Clone, Eq, PartialEq, Hash)]
+pub enum ReplaceKeyErr {
+    #[error("replace_key: the old key does not exist")]
+    /// The old key does not exist.
+    OldKeyNotExist,
+    #[error("replace_key: the new key is already occupied")]
+    /// The new key is already occupied.
+    NewKeyOccupied,
+}
 
 /// Some general extensions to `Maps` (such as `HashMap`, `BTreeMap`, `IndexMap`).
 pub trait MapExt<K, Q: ?Sized = K> {
-    /// Replace an existing key with a new one.
+    /// Replace an existing key with a new (non-existing) one.
     ///
-    /// If the key exists and is successfully replaced, return true;
-    /// otherwise, return false.
-    fn replace_key(&mut self, k1: &Q, k2: K) -> bool
+    /// If k1 does not exist, return `Err(ReplaceKeyErr::OldKeyNotExist)`.
+    ///
+    /// If k1 exists and k2 also exists, return `Err(ReplaceKeyErr::NewKeyOccupied)`.
+    ///
+    /// Otherwise, return `Ok(())` after the replacement is completed.
+    fn replace_key(&mut self, k1: &Q, k2: K) -> Result<(), ReplaceKeyErr>
     where
         K: Borrow<Q>;
 }
@@ -21,13 +36,18 @@ where
     Q: Hash + Eq + ?Sized,
     S: BuildHasher,
 {
-    fn replace_key(&mut self, k1: &Q, k2: K) -> bool {
-        if let Some(v) = self.remove(k1) {
-            self.insert(k2, v);
-            true
-        } else {
-            false
+    fn replace_key(&mut self, k1: &Q, k2: K) -> Result<(), ReplaceKeyErr> {
+        if !self.contains_key(k1) {
+            return Err(ReplaceKeyErr::OldKeyNotExist);
         }
+
+        if self.contains_key(k2.borrow()) {
+            return Err(ReplaceKeyErr::NewKeyOccupied);
+        }
+
+        let v = self.remove(k1).expect("this should be unreachable");
+        self.insert(k2, v);
+        Ok(())
     }
 }
 
@@ -36,13 +56,18 @@ where
     K: Borrow<Q> + Ord,
     Q: Ord + ?Sized,
 {
-    fn replace_key(&mut self, k1: &Q, k2: K) -> bool {
-        if let Some(v) = self.remove(k1) {
-            self.insert(k2, v);
-            true
-        } else {
-            false
+    fn replace_key(&mut self, k1: &Q, k2: K) -> Result<(), ReplaceKeyErr> {
+        if !self.contains_key(k1) {
+            return Err(ReplaceKeyErr::OldKeyNotExist);
         }
+
+        if self.contains_key(k2.borrow()) {
+            return Err(ReplaceKeyErr::NewKeyOccupied);
+        }
+
+        let v = self.remove(k1).expect("this should be unreachable");
+        self.insert(k2, v);
+        Ok(())
     }
 }
 
@@ -53,24 +78,54 @@ mod tests {
     #[test]
     fn replace_key_hashmap() {
         let mut map = HashMap::new();
+
         map.insert("k1".to_string(), 123);
+        map.insert("k2".to_string(), 456);
 
         assert_eq!(map["k1"], 123);
-        assert!(!map.replace_key("k2", "k2".to_string()));
-        assert!(map.replace_key("k1", "k2".to_string()));
+        assert_eq!(map["k2"], 456);
+        assert_eq!(
+            map.replace_key("k3", "k2".to_string()),
+            Err(ReplaceKeyErr::OldKeyNotExist)
+        );
+        assert_eq!(
+            map.replace_key("k3", "k4".to_string()),
+            Err(ReplaceKeyErr::OldKeyNotExist)
+        );
+        assert_eq!(
+            map.replace_key("k1", "k2".to_string()),
+            Err(ReplaceKeyErr::NewKeyOccupied)
+        );
+        assert_eq!(map.replace_key("k1", "k3".to_string()), Ok(()));
         assert!(!map.contains_key("k1"));
-        assert_eq!(map["k2"], 123);
+        assert_eq!(map["k3"], 123);
+        assert_eq!(map["k2"], 456);
     }
 
     #[test]
     fn replace_key_btreemap() {
         let mut map = BTreeMap::new();
+
         map.insert("k1".to_string(), 123);
+        map.insert("k2".to_string(), 456);
 
         assert_eq!(map["k1"], 123);
-        assert!(!map.replace_key("k2", "k2".to_string()));
-        assert!(map.replace_key("k1", "k2".to_string()));
+        assert_eq!(map["k2"], 456);
+        assert_eq!(
+            map.replace_key("k3", "k2".to_string()),
+            Err(ReplaceKeyErr::OldKeyNotExist)
+        );
+        assert_eq!(
+            map.replace_key("k3", "k4".to_string()),
+            Err(ReplaceKeyErr::OldKeyNotExist)
+        );
+        assert_eq!(
+            map.replace_key("k1", "k2".to_string()),
+            Err(ReplaceKeyErr::NewKeyOccupied)
+        );
+        assert_eq!(map.replace_key("k1", "k3".to_string()), Ok(()));
         assert!(!map.contains_key("k1"));
-        assert_eq!(map["k2"], 123);
+        assert_eq!(map["k3"], 123);
+        assert_eq!(map["k2"], 456);
     }
 }
