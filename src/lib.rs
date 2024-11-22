56d962dcb6c021837cd128b14de5a67dd34f89c1
29 <791603901@qq.com>
2024-11-22 09:53:07 +0800

    📝 Optimize documents

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 #![cfg_attr(nightly, feature(doc_auto_cfg))]
 
+//! **E**xtensions for the rust **S**tandard library and **T**okio.
+
 /// Extensions to the [`std::collections`](https://doc.rust-lang.org/stable/std/collections/index.html) module.
 pub mod collections;
 /// Extensions to the [`std::result`](https://doc.rust-lang.org/stable/std/result/index.html) module.
